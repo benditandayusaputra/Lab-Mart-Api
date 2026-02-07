768e839a7f3fdcb79c986d51b8111d3df674d874
benditandayusaputra <benditandayusaputra@gmail.com>
2026-02-07 23:56:20 +0700

    update server.js

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 export default app;
