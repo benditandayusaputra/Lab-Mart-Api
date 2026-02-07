f3e8826208fd1dfd98df039710656ad4467eb965
benditandayusaputra <benditandayusaputra@gmail.com>
2026-02-07 23:06:42 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import authRoutes from './routes/authRoutes.js';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -20,10 +20,10 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
